a1f2d1e6ab8124b900789652038dcd9751f88253
EaswaraKrishnanS <easwarakrishnan2003@gmail.com>
2024-11-15 20:43:52 +0530

    text

---
+++ Message.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+      <h1>Hi i am Easwar</h1>
+    </div>
+  )
+}
+
+export default Message
